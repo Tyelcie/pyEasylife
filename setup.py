5f5a9b6475867e25f3ddfd1be4dab75943818892
Tyelcie <tyelcie@gmail.com>
2019-01-18 00:44:58 +0800

    setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     # Needed to actually package something
     packages=['stobserver'],
     # Needed for dependencies
-    install_requires=['pandas', 're'],
+    install_requires=['pandas'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
