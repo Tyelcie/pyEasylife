a867fe6b1d2f8b84b94325262da777045c0aa126
Tyelcie <tyelcie@gmail.com>
2019-01-20 16:21:07 +0800

    modify structure

--- stobserver/assign_status.py
+++
@@ -1,5 +0,0 @@
-def assign_status(x, student, projects, status):
-    students_idx = x[x['Name'].isin([student])].index
-    projects_idx = x[projects].columns
-    x.loc[students_idx, projects_idx] = status
-    return x
