b6013ea8fb783e0ec5e36fd5b48e559ccbfda7c4
Tyelcie <tyelcie@gmail.com>
2019-03-14 11:28:57 +0800

    timing and checking functions

--- stanager/scheduling.py
+++ stanager/scheduling.py
@@ -1,4 +1,24 @@
 name = 'scheduling'
 
-def demo():
-    print('hello world!')
+def parse_time(time):
+    parsed = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M')
+    return parsed
+
+class Classes:
+    def __init__(self, name, duration):
+        self.name = name
+        self.duration = duration
+
+def check_conflict(previous, proposal, rest):
+    '''previous: list of tuples enveloping start time and end time
+       current: list of tuples enveloping start time and end time'''
+    buffle = datetime.timedelta(minutes = rest)
+    checked = [not (prvs[0]>= proposal[1] + buffle or prvs[1] <= proposal[0] - buffle) for prvs in previous]
+    conflict = list(compress(range(len(checked)), checked))
+    conf_class = [previous[i] for i in conflict]
+    return checked, conf_class
+
+def check_available(available, proposal):
+    checked = [avlb[0] <= proposal[0] and avlb[1] >= proposal[1] for avlb in available]
+    occupation = list(compress(range(len(checked)), checked))
+    return checked, occupation
