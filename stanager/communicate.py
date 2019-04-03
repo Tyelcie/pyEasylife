dbefce31c210f5cfc66c9181f1f650e14ec898f2
Tyelcie <tyelcie@gmail.com>
2019-04-03 17:19:30 +0800

    Update communicate.py

--- stanager/communicate.py
+++ stanager/communicate.py
@@ -3,7 +3,7 @@ import pandas as pd
 import itchat as ic
 
 def send_task_group(task_seq, msg_template, students_df):
-    for i in range(1, len(students_df)):
+    for i in range(0, len(students_df)):
         student = students_df['姓名'][i]
         userID = ic.search_friends(name=student)[0]['UserName']
         personal_task = students_df[task_seq][i]
